7b20d55c8ec7738fa10133b72ba0bf8d24b2b239
nissaura <cnissara.almida@outlook.com>
2022-06-07 18:02:08 -0300

    changes

---
+++ 2.bancodedados/dml.sql
@@ -0,0 +1,10 @@
+create database VILAOINFOC;
+
+use VILAOINFOC;
+
+CREATE TABLE TB_VILAO(
+ID_VILAO             int primary
+NM_VILAO             varchar(10)
+DS_MALDADES          varchar(50)
+BT_SUPER_PODERES     boolean
+)			 
--- api/index.js
+++ api/index.js
@@ -8,6 +8,13 @@ server.use(cors());
 
 server.use(express.json());
 
+server.get('/vilao', async (req, resp) =>{
+    let vilao = req.vilao(
+        resp.send
+    )
+    
+})
+
 
 server.listen(process.env.PORT, 
                 () => console.log(` API ONLINE NA PORTA ${process.env.PORT}`));
