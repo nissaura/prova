f3b06ebb3eaa8658fa53f244e5d3c549bd142d5c
nissaura <cnissara.almida@outlook.com>
2022-06-07 18:15:02 -0300

    changes1

---
+++ 2.bancodedados/ddl.sql
@@ -0,0 +1,4 @@
+insert into TB_VILAO (nm_vilao, ds_maldades, bt_super_poder)
+		Values('Rainha má', 'Foi malvada com alguma princesa', false);
+        
+ SELECT FROM        
--- 2.bancodedados/dml.sql
+++ 2.bancodedados/dml.sql
@@ -3,8 +3,8 @@ create database VILAOINFOC;
 use VILAOINFOC;
 
 CREATE TABLE TB_VILAO(
-ID_VILAO             int primary
-NM_VILAO             varchar(10)
-DS_MALDADES          varchar(50)
+ID_VILAO             int primary key auto_increment, 
+NM_VILAO             varchar(10),
+DS_MALDADES          varchar(50),
 BT_SUPER_PODERES     boolean
 )			 
